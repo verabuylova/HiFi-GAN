b4a02953d617f1c83a233fdc27d8396fe82314c5
Вера <vera@Vera--Notebook.local>
2024-11-27 00:40:22 +0300

    add dataset

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,6 +1,5 @@
 import torch
 
-
 def collate_fn(dataset_items: list[dict]):
     """
     Collate and pad fields in the dataset items.
@@ -13,13 +12,7 @@ def collate_fn(dataset_items: list[dict]):
         result_batch (dict[Tensor]): dict, containing batch-version
             of the tensors.
     """
+    wavs = torch.stack([item["wav"] for item in dataset_items])
+    specs = torch.stack([item["spec"] for item in dataset_items])
 
-    result_batch = {}
-
-    # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
-    )
-    result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
-
-    return result_batch
+    return {"wav": wavs, "spec": specs}
