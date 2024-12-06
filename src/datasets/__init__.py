edf32f50cae85ff65adece8623871cdcd27fb6af
verabuylova <vbujlova@nes.ru>
2024-12-06 13:24:11 +0300

    add custom dataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,2 +1,4 @@
 from src.datasets.ljspeech import LJspeechDataset
 from src.datasets.ljspeech_kaggle import LJspeechDatasetKaggle
+from src.datasets.custom_audio_dataset import CustomDirAudioDataset
+from src.datasets.custom_text_dataset import CustomDirTextDataset
