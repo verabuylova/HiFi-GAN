5d194bb980c64f80977dde0a1a6951addd13fdc0
verabuylova <vbujlova@nes.ru>
2024-12-08 19:58:17 +0300

    update text dataset

--- src/datasets/custom_text_dataset.py
+++ src/datasets/custom_text_dataset.py
@@ -6,12 +6,15 @@ from src.datasets.base_dataset import BaseDataset
 class CustomDirTextDataset(BaseDataset):
     def __init__(self, data_dir, *args, **kwargs):
         data = []
-        transcription_dir = Path(data_dir) / "transcriptions"
+        transcription_dir = Path(data_dir)
         for path in Path(transcription_dir).iterdir():
-            entry = {"path": path, "text": None, "audio_len": 0}
+            entry = {}
+            entry["path"] = str(path)
+            entry["text"] = None
+            entry["audio_len"] = 0
             if path.suffix in [".txt"]:
                 with path.open() as f:
                     entry["text"] = f.read().strip()
-                if entry["text"] is not None:
-                    data.append(entry)
+            if len(entry) > 0:
+                data.append(entry)
         super().__init__(data, *args, **kwargs)
